2e8a4e1925c395a4ffb14f8aaab0492a8e67a0f4
HaraYoung <qkrtpdud9899@gmail.com>
2022-01-25 00:18:21 +0900

    데이터타입까지

--- 02-Javascript/데에터타입.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-//Number
-let sample1 = 123;
-console.log(typeof sample1);
-
---
+++ 02-Javascript/데이터타입.js
@@ -0,0 +1,27 @@
+"use strict"
+
+/*중요*/
+//Number
+let sample1 = 123;
+console.log(typeof sample1);
+
+//Boolean : 논리값-true(참),false(거짓)만 표현 가능
+let sample2 = true;
+console.log(typeof sample2);
+
+//쌍,홑 따옴표로 감싼 경우 문자열로 인식-문장,글자의 집합
+let sample3 = "hello world"
+console.log(typeof sample3);
+
+/*천천히 익힐 타입들*/
+//Object -객체
+let sample4 = new Date();
+console.log(typeof sample4);
+
+//null 비어있는 객체(object)-아무런 기능이 없는 상태
+let sample5 = null;
+console.log(typeof sample4);
+
+//Undefined- 선언만 하고 값이 정의되지 않은 상태
+let sample6;
+console.log(typeof sample6);
