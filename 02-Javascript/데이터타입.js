7b330866e3c564fc9930c1da0c0a07fb684aab79
HaraYoung <qkrtpdud9899@gmail.com>
2022-01-24 23:50:59 +0900

    뭔가이상하다;

---
+++ 02-Javascript/데이터타입.js
@@ -0,0 +1,4 @@
+"use strict"
+
+let sampleValue1 = 123;
+console.log(typeof sampleValue1);
--- 02-Javascript/상수.js
+++ 02-Javascript/상수.js
@@ -3,4 +3,4 @@
 const a = 123;
 console.log(a);
 
-a = 234; //const로 선언된 변수라 값 변경x
+//a = 234; //const로 선언된 변수라 값 변경x
