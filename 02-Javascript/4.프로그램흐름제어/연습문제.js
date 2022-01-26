bcce546515832ee72976c7849bdfeb33f13cc85d
HaraYoung <qkrtpdud9899@gmail.com>
2022-01-26 19:12:30 +0900

    연습문제 풀었다

--- 02-Javascript/4.프로그램흐름제어/연습문제.js
+++ 02-Javascript/4.프로그램흐름제어/연습문제.js
@@ -1,10 +1,10 @@
 const 수학 = "B";
 
-if (수학="A"){
+if (수학=="A") {
     console.log("이 과목을 Pass 했습니다.");
-} else if (수학 ="B") {
+} else if (수학 =="B") {
     console.log("이 과목을 Pass 했습니다.");
-} else if (수학 ="C") {
+} else if (수학 =="C") {
     console.log("이 과목을 Pass 했습니다.");
 } else {console.log("이 과목을 Pass하지 못했습니다.")
 }
