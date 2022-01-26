a22f28c59a0cb5cb7686c8f68d2a81b1c9fc5918
HaraYoung <qkrtpdud9899@gmail.com>
2022-01-27 02:44:22 +0900

    기본문법 활용 예제까지

--- 02-Javascript/5.기본문법활용/01-변수의스코프.js
+++ 02-Javascript/5.기본문법활용/01-변수의스코프.js
@@ -17,3 +17,33 @@ if (true) {
   console.log("블록안:" + num2);
 }
 console.log("블록밖" + num2);
+
+//let 중복선언
+let num3 =100;
+ if (true) {
+   // 블록 밖에서 생성된 변수를 블록안에서 사용o
+   let num4 = num3 +100 ;
+   console.log("블록안:" + num4);
+ }
+
+ /*let으로 선언된 변수는 if문의 실행 여부와 상관없이 
+ 블록을 빠져나올수 없음 ->에러*/
+ console.log("블록밖:" + num4);
+
+ //for문의 초기식을 var로 선언
+ for (var i=0; i<10; i++) {
+   console.log("반복문 안:"+ i);
+ }
+ console.log("반복문 밖:" + i);
+
+ //for문의 초기식을 let으로 선언
+ for (let j=0; j<10; j++){
+   console.log("반복문 안:"+j)
+ }
+ //for문의 초기식도 {}에 속한것으로 봄-> j값은 for블록 밖으로 못나옴.
+ console.log("반복문 밖:" + j);
+
+ //var 키워드는 할당 후 선언 가능
+ y= 200;
+ var y;
+ console.log(y);
